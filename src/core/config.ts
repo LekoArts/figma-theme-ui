9715ff544ad840ef5d3849695c26ebd297481777
LekoArts <lekoarts@gmail.com>
2020-01-11 23:16:46 +0100

    fix: Regex so that whitespace inside double quotes doesn't get deleted. Important for fonts

--- src/core/config.ts
+++ src/core/config.ts
@@ -4,7 +4,7 @@ import { Theme } from 'theme-ui'
 export const parseConfig = (config: string): Theme => {
   try {
     // Remove any whitespace
-    config = config.replace(/\s/g, '')
+    config = config.replace(/("[^"\\]*(?:\\.[^"\\]*)*")|\s+/gm, '$1')
     // Backticks to double quotes
     config = config.replace(/`/g, '"')
     // Remove anything before module exports
