4870abceab3de6c9085b8505e876c28e68f4edb1
Lennart <lekoarts@gmail.com>
2021-04-13 11:56:06 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     `plugin:@typescript-eslint/recommended`,
     `plugin:import/typescript`,
     `plugin:prettier/recommended`,
-    `prettier/@typescript-eslint`,
   ],
   plugins: [`@typescript-eslint`, `prettier`],
   parserOptions: {
